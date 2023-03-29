276df91fdba0d88cbf6b0ce59df93df80c327cfc
Edwar Andres Pineda <contractor_edwar.pineda@educationadvanced.com>
2023-03-29 08:37:39 -0500

    Commit 1: Blazor WASM walking skeleton

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/value
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct(int id)
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
